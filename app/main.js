941fe67baffa45ed60389227dee624aa81b5f67e
Schiktor <Meremkulov.marat@yandex.ru>
2025-06-24 23:00:48 +0300

    adding a random circle

--- app/main.js
+++ app/main.js
@@ -2,8 +2,9 @@ const start = document.querySelector('#start');
 const screen = document.querySelectorAll('.screen');
 const btnTime = document.querySelectorAll('.time-btn');
 const timeList = document.querySelector('.time-list');
-let timeDisplay = document.querySelector('#time')
-let time = 20;
+const board =document.querySelector('.board');
+let timeDisplay = document.querySelector('#time');
+let time = 10;
 
 start.addEventListener('click',(event) => {
 event.preventDefault()
@@ -14,14 +15,15 @@ timeList.addEventListener('click',(event) => {
     if(event.target.classList.contains('time-btn')) {
         time = parseInt(event.target.getAttribute('data-time'));
         screen[1].classList.add('up')
-        startGame()
     }
 })
 
+startGame()
 // game launch function
 
 function startGame() {
     setInterval(decreaseTime, 1000)
+    createRandomCircle()
     setTime(time)
 }
 
@@ -39,4 +41,18 @@ function decreaseTime () {
 
 function setTime(value) {
     timeDisplay.innerHTML= `00:${value}`
+}
+
+function createRandomCircle() {
+    const circle = document.createElement('div');
+    circle.classList.add('circle')
+    const size = getRandomSize(10,60)
+
+    circle.style.width=`${size}px`
+    circle.style.height=`${size}px`
+    board.append(circle)
+}
+
+function getRandomSize(min, max) {
+    return Math.round(Math.random() * (max-min) + min)
 }
